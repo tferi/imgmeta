70be22a0213a4e8f96efeadff079036de7f295fa
Ferenc Toth <tothfj@gmail.com>
2019-02-09 14:03:51 +0100

    optimize

--- src/main/scala/com/tothferenc/imgmeta/datasource/DirectoryDataSource.scala
+++ src/main/scala/com/tothferenc/imgmeta/datasource/DirectoryDataSource.scala
@@ -1,35 +1,38 @@
 package com.tothferenc.imgmeta.datasource
 
-import java.io.{BufferedInputStream, File, FileFilter, FileInputStream}
+import java.io.{BufferedInputStream, FileInputStream}
 import java.nio.file.Path
 
 import akka.NotUsed
 import akka.stream.Materializer
-import akka.stream.scaladsl.{FileIO, Source, StreamConverters}
+import akka.stream.scaladsl.Source
 import com.tothferenc.imgmeta.model.{Album, Image, StreamIn}
 
-import scala.concurrent.duration._
 
 final case class DirectoryDataSource(root: Path)(implicit m: Materializer) extends DataSource {
   private val dataSourceName = root.toString
 
-  private def getFilePaths(path: Path): Source[Path, NotUsed] = {
+  private case class Input(path: Path, numFilesInDir: Option[Int])
+
+  private def getFilePaths(path: Path): Source[Input, NotUsed] = {
     val (dirs, rest) = path.toFile.listFiles().partition(_.isDirectory)
     val thisAlbum =
       if (rest.nonEmpty)
-        Source.single(path).concat(Source.fromIterator(() => rest.iterator.map(_.toPath)))
+        Source.single(Input(path, Some(rest.length))).concat(Source.fromIterator(() => rest.iterator.map(f => Input(f.toPath, None))))
       else
         Source.empty
     dirs.foldLeft(thisAlbum)((s, f) => s concat Source.lazily(() => getFilePaths(f.toPath)))
   }
 
-  override def getImageFiles(): Source[StreamIn, NotUsed] = getFilePaths(root).mapConcat { filePath =>
-    if (filePath.toFile.isDirectory) {
-
-      val filesInDir = filePath.toFile.listFiles(new FileFilter {
-        override def accept(pathname: File): Boolean = pathname.isFile
-      }).length // it's not optimal that we crawl the dir twice, but it'll do for now.
-
+  override def getImageFiles(): Source[StreamIn, NotUsed] = getFilePaths(root).mapConcat { in =>
+    val filePath = in.path
+    in.numFilesInDir.fold[List[StreamIn]] {
+      List(StreamIn.Elem(Image(
+        dataSourceName,
+        root.relativize(filePath.getParent).toString,
+        filePath.getFileName.toString,
+        () => new BufferedInputStream(new FileInputStream(filePath.toFile), 8192 * 128))))
+    } { filesInDir =>
       if (filesInDir == 0)
         Nil
       else
@@ -38,10 +41,5 @@ final case class DirectoryDataSource(root: Path)(implicit m: Materializer) exten
           root.relativize(filePath).toString,
           Some(filesInDir))))
     }
-    else List(StreamIn.Elem(Image(
-      dataSourceName,
-      root.relativize(filePath.getParent).toString,
-      filePath.getFileName.toString,
-      () => new BufferedInputStream(new FileInputStream(filePath.toFile), 8192 * 128 ))))
   }
 }
